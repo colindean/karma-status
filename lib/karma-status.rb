bc6255da18066cb48ccdfba893b67c5f225805e8
Colin Dean <git@cad.cx>
2013-12-15 13:42:49 -0500

    initial working retrieval

---
+++ lib/karma-status.rb
@@ -0,0 +1,51 @@
+require 'bundler'
+require 'json'
+require 'logger'
+Bundler.require
+
+module Karma
+  LOGGER = ::Logger.new "karma.log"
+  module Dashboard
+  end
+  class Hotspot
+
+    def self.status
+      Karma::Hotspot::Status.new
+    end
+  end
+end
+
+class Karma::Dashboard::Status
+end
+
+class Karma::Hotspot::Status
+
+  STATUS_URL = 'https://hotspot.yourkarma.com/api/status.json'
+
+  def initialize
+    @results = nil
+    @promise = Concurrent::Promise.new { update! }
+  end
+
+  def get
+    @results ||= update!
+  end
+
+  def update!
+    @results = retrieve
+  end
+
+  def retrieve
+    response = ::RestClient.get STATUS_URL do |response, request, result, &block|
+      case response.code
+      when 200
+        Karma::LOGGER.info "success"
+        Karma::LOGGER.info response
+        response
+      else
+        response.return!(request, result, &block)
+      end
+    end
+    ::JSON.parse response.to_str
+  end
+end
